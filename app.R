a7747f7a970be95339af4186896a607a0992345c
hanastrickland <strickh@uw.edu>
2018-05-19 10:20:34 -0700

    change race column name

--- app.R
+++ app.R
@@ -11,98 +11,98 @@ counties <- read.csv("data/evictionlab-us-counties.csv", stringsAsFactors = FALS
 
 
 # WA counties
-counties <- counties %>% 
-  filter(parent.location == "Washington") 
+counties <- counties %>%
+  filter(parent.location == "Washington")
 
-counties_long <- gather(counties, 
-                        key = pct.by.race,  
-                        value = pct.pop, 
+counties_long <- gather(counties,
+                        key = race,
+                        value = pct.pop,
                         c(pct.white:pct.other )
                         )
 
 years <- unique(counties_long$year)
-races <- unique(counties_long$pct.by.race)
+races <- unique(counties_long$race)
 
 eviction_rate_range <- range(counties_long$eviction.rate, na.rm = TRUE)
 
+
 ui <- fluidPage(
   titlePanel("Eviction Rates"),
-  
+
   sidebarLayout(
     #selectInput("year", label = "Select Year", choices = years),
     selectInput("race", label = "Select Race", choices = races),
-    
+
     sliderInput("eviction_rate",
                 label = "Eviction Rate",
                 min = eviction_rate_range[1],
                 max = eviction_rate_range[2],
                 value = eviction_rate_range
-    ), 
-    
+    ),
+
   ),
-  
+
   mainPanel(
     tabsetPanel(type = "tabs",
                 tabPanel("Plot", plotOutput("plot", click = "plot_click"),
                          p("Highlighting:", strong(textOutput("selected", inline = TRUE)))),
                 tabPanel("Table", dataTableOutput("table")),
-                tabPanel("Dataset Information", 
-                         p("This research uses data from The Eviction Lab at Princeton University, 
-                           a project directed by Matthew Desmond and designed by Ashley Gromis, 
-                           Lavar Edmonds, James Hendrickson, Katie Krywokulski, Lillian Leung, 
-                           and Adam Porton. The Eviction Lab is funded by the JPB, Gates, 
-                           and Ford Foundations as well as the Chan Zuckerberg Initiative. 
+                tabPanel("Dataset Information",
+                         p("This research uses data from The Eviction Lab at Princeton University,
+                           a project directed by Matthew Desmond and designed by Ashley Gromis,
+                           Lavar Edmonds, James Hendrickson, Katie Krywokulski, Lillian Leung,
+                           and Adam Porton. The Eviction Lab is funded by the JPB, Gates,
+                           and Ford Foundations as well as the Chan Zuckerberg Initiative.
                            More information is found at evictionlab.org."))
                          )
-    
+
                          )
-  
+
                 )
 
 server <- function(input, output) {
-  
+
   data <- reactiveValues()
   data$selected_year <- ""
-  
+
   output$plot <- renderPlot({
-    
+
     results_data <- counties_long %>%
       filter(eviction.rate >= input$eviction_rate[1] & eviction.rate <= input$eviction_rate[2])
-    
-    #results_data <- counties[counties$year == input$year,  ]
-    results_data <- counties_long[counties_long$pct.by.race == input$race, ]
-    
+
+    results_data <- counties_long[counties_long$race == input$race, ]
+
     ggplot(data = results_data, mapping = aes(x = poverty.rate, y = eviction.rate)) +
-      geom_point(aes(color = (year %in% data$selected_year))) +
+      geom_point(aes(color = (year %in% data$selected_year), size = 5)) +
       guides(color = FALSE)
-    
+
   })
-  
+
   output$table <- renderDataTable({
     results_data <- counties_long %>%
       filter(eviction.rate >= input$eviction_rate[1] & eviction.rate <= input$eviction_rate[2])
-    
-    #results_data <- counties[counties$year == input$year,  ]
-    results_data <- counties_long[counties_long$pct.by.race == input$race, ]
+
+    results_data <- counties_long[counties_long$race == input$race, ]
     results_data
-    
-    
+
+
   })
-  
+
   output$selected <- renderText({
     data$selected_year
-    
+
   })
-  
+
   observeEvent(input$plot_click, {
     selected <- nearPoints(counties_long, input$plot_click)
     data$selected_year <- unique(selected$year)
-    
+
   })
-  
+
 }
 
 
-
 shinyApp(ui, server)
+
+
 
