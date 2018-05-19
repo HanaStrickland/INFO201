2cef3e5fec71a72b6ae61b031b1c55cc08c0c659
hanastrickland <strickh@uw.edu>
2018-05-18 23:08:45 -0700

    add tabs

--- app.R
+++ app.R
@@ -3,49 +3,50 @@ library(ggplot2)
 library(dplyr)
 library(maps)
 
+
 ### Read in and Clean Data
 
 counties <- read.csv("data/evictionlab-us-counties.csv", stringsAsFactors = FALSE)
-cities <- read.csv("data/cities.csv", stringsAsFactors = FALSE) # cities in WA
 
 
+# WA counties
 counties <- counties %>% 
-  filter(parent.location == "Washington") # WA counties
+  filter(parent.location == "Washington") 
 
-cities <- cities %>% 
-  filter(name == "Seattle")
+years <- unique(counties$year)
 
-year_range <- range(counties$year)
 eviction_rate_range <- range(counties$eviction.rate, na.rm = TRUE)
-
 
 ui <- fluidPage(
   titlePanel("Eviction Rates"),
   
   sidebarLayout(
-    sliderInput("year",
-                label = "Year from 2000 to 2016",
-                min = year_range[1],
-                max = year_range[2],
-                value = year_range
-    ),
+    selectInput("year", label = "Select Year", choices = years),
+    
     sliderInput("eviction_rate",
                 label = "Eviction Rate",
                 min = eviction_rate_range[1],
                 max = eviction_rate_range[2],
                 value = eviction_rate_range
-    )
+    ), 
+
   ),
   
   mainPanel(
-    plotOutput("plot", click = "plot_click"),
-    textOutput("selected")
-    #p("Highlighting:", strong(textOutput("selected", inline = TRUE)))
-    
+    tabsetPanel(type = "tabs",
+                tabPanel("Plot", plotOutput("plot", click = "plot_click"),
+                         p("Highlighting:", strong(textOutput("selected", inline = TRUE)))),
+                tabPanel("Table", dataTableOutput("table")),
+                tabPanel("Dataset Information", 
+                         p("This research uses data from The Eviction Lab at Princeton University, 
+                           a project directed by Matthew Desmond and designed by Ashley Gromis, 
+                           Lavar Edmonds, James Hendrickson, Katie Krywokulski, Lillian Leung, 
+                           and Adam Porton. The Eviction Lab is funded by the JPB, Gates, 
+                           and Ford Foundations as well as the Chan Zuckerberg Initiative. 
+                           More information is found at evictionlab.org."))
+                )
+  
   )
-  
-  
-  
   
 )
 
@@ -55,29 +56,36 @@ server <- function(input, output) {
   data$selected_year <- ""
   
   output$plot <- renderPlot({
-  
-    results_data <- counties %>%
-      filter(year > input$year[1] & year < input$year[2], 
-             eviction.rate > input$eviction_rate[1] & eviction.rate < input$eviction_rate[2]) 
     
+    results_data <- counties %>%
+      filter(eviction.rate >= input$eviction_rate[1] & eviction.rate <= input$eviction_rate[2])
+    
+    results_data <- counties[counties$year == input$year,  ]
+
     ggplot(data = results_data, mapping = aes(x = poverty.rate, y = eviction.rate)) +
       geom_point(aes(color = (year %in% data$selected_year))) +
       guides(color = FALSE)
     
+  })
+  
+  output$table <- renderDataTable({
+    results_data <- counties %>%
+      filter(eviction.rate >= input$eviction_rate[1] & eviction.rate <= input$eviction_rate[2])
+    
+    results_data <- counties[counties$year == input$year,  ]
+    results_data
+
     
   })
   
   output$selected <- renderText({
     data$selected_year
     
-    
   })
   
   observeEvent(input$plot_click, {
     selected <- nearPoints(counties, input$plot_click)
     data$selected_year <- unique(selected$year)
-    
-    
     
   })
   
