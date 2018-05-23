1677786b6990a0ddc28140a2f5a4f0ba7c9dcda1
hanastrickland <strickh@uw.edu>
2018-05-22 19:57:44 -0700

    add side pannel and reactive for year and eviction rate

--- my_server.R
+++ my_server.R
@@ -5,22 +5,27 @@ server <- function(input, output) {
   data$selected_county <- ""
   data$selected_info <- ""
   
+  results_data <- reactive({
+    
+    results <- counties[counties$year == input$year, ]
+    
+    results <- results %>%
+      filter(eviction.rate >= input$eviction_rate[1] & eviction.rate <= input$eviction_rate[2])
+    
+    results
+    
+    
+  })
+  
   
   output$plot <- renderPlot({
     
-    # select year
-    results_data <- counties[counties$year == input$year, ] 
-    
-    #filter for eviction rate
-    results_data <- results_data %>%
-      filter(eviction.rate >= input$eviction_rate[1] & eviction.rate <= input$eviction_rate[2])
-    
-    ggplot(data = results_data, mapping = aes(x = rent.burden, y = eviction.rate)) +
+    ggplot(data = results_data(), mapping = aes(x = rent.burden, y = eviction.rate)) +
       geom_point(aes(color = (name %in% data$selected_county)), size = 3) +
       guides(color = FALSE) +
-      labs(title = "Relationship Between Rent Burden and Eviction Rate") +
+      labs(title = "Relationship Between Rent Burden and Eviction Rate in WA Counties") +
       
-      xlab("Rent Burden") +
+      xlab("Rent Burden (%)") +
       ylab("Eviction Rate") +
       theme(plot.title = element_text(size = 20), 
             axis.title.x = element_text(size = 15),  
@@ -31,10 +36,7 @@ server <- function(input, output) {
   
   output$table <- renderDataTable({
     
-    results_data <- counties[counties$year == input$year, ]
-    
-    results_data <- results_data %>%
-      filter(eviction.rate >= input$eviction_rate[1] & eviction.rate <= input$eviction_rate[2])
+    results_data <- results_data()
     
     colnames(results_data) <- c("Year", "County", "Population", "Poverty Rate", "Rent Burden", "Eviction Rate")
     
@@ -57,7 +59,7 @@ server <- function(input, output) {
     selected <- nearPoints(counties, input$plot_click)
     selected <- filter(selected, year == input$year) # filters for year picked in the dropdown menu
     
-    selected <- as.vector(selected)
+    selected <- as.vector(selected)  
     
     data$selected_county <- paste(selected$name) # used to highlight datapoint selected in a different color
 
--- my_ui.R
+++ my_ui.R
@@ -13,52 +13,71 @@ ui <- fluidPage(
   titlePanel("Eviction Rates in Washington by County"),
   
   sidebarLayout(
+    sidebarPanel(
+      
+      # widget selects year
+      selectInput("year", label = "Select Year", choices = years), 
+      
+      # widget filters for eviction rate
+      sliderInput("eviction_rate",
+                  label = "Eviction Rate",
+                  min = eviction_rate_range[1],
+                  max = eviction_rate_range[2],
+                  value = eviction_rate_range
+      )
+      
+    ),
     
-    # widget selects year
-    selectInput("year", label = "Select Year", choices = years), 
     
-    # widget filters for eviction rate
-    sliderInput("eviction_rate",
-                label = "Eviction Rate",
-                min = eviction_rate_range[1],
-                max = eviction_rate_range[2],
-                value = eviction_rate_range
-    )
-    
-  ),
-  
-  mainPanel(
-    tabsetPanel(type = "tabs",
-                tabPanel("Plot", p("This plot shows the relationship between rent burden
+    mainPanel(
+      tabsetPanel(type = "tabs",
+                  tabPanel("Plot", p("This plot shows the relationship between rent burden
                                    (the percentage of income spent on rent) and eviction rate for", 
-                                   strong(textOutput("selected_year_plot", inline = TRUE)), ". There seems to be a 
+                                     strong(textOutput("selected_year_plot", inline = TRUE)), 
+                                     ". Each data point represents a county. You can select a different 
+                                   year using the dropdown menu above, and you can filter for a different 
+                                   range of eviction rates using the slider above. There seems to be a 
                                    low to negligible correlation between rent burden and eviction rate."
-                ), 
-                plotOutput("plot", click = "plot_click"),
-                p(htmlOutput("selected", inline = TRUE)
+                                     
+                  ), 
+                  p("Click on a data point to learn more about it."),
+                  
+                  plotOutput("plot", click = "plot_click"),
+                  
+                  p(htmlOutput("selected", inline = TRUE)
                   )
-              
-                ),
-                
-                tabPanel("Table", 
-                         p("This table shows a county's population, poverty rate, rent burden, and eviction rate for",
-                           strong(textOutput("selected_year_table", inline = TRUE)), ". You can select a different year
+                  
+                  ),
+                  
+                  tabPanel("Table", 
+                           p("This table shows a county's population, poverty rate, rent burden, and eviction rate for",
+                             strong(textOutput("selected_year_table", inline = TRUE)), ". You can select a different year
                            using the dropdown menu above, and you can filter for a different range of eviction rates
                            using the slider above."
-                         ),
-                         dataTableOutput("table")
-                ),
-                
-                tabPanel("Dataset Information",
-                         p("This research uses data from The Eviction Lab at Princeton University,
+                           ),
+                           dataTableOutput("table")
+                  ),
+                  
+                  tabPanel("Dataset Information",
+                           h3("About the Data"),
+                           p("This research uses data from The Eviction Lab at Princeton University,
                            a project directed by Matthew Desmond and designed by Ashley Gromis,
                            Lavar Edmonds, James Hendrickson, Katie Krywokulski, Lillian Leung,
                            and Adam Porton. The Eviction Lab is funded by the JPB, Gates,
                            and Ford Foundations as well as the Chan Zuckerberg Initiative.
                            More information is found at", 
-                           a("evictionlab.org", href="https://data-downloads.evictionlab.org"), ".")) 
-                
-                
+                             a("evictionlab.org", href="https://data-downloads.evictionlab.org"), "."),
+                           h3("Terms"),
+                           p(strong("Poverty Rate:"),  "% of the population with income in the past 12 months 
+                             below the poverty level"),
+                           p(strong("Rent Burden:"), "Median gross rent as a percentage of household income"),
+                           p(strong("Eviction Rate:"), "Ratio of the number of renter-occupied households in an 
+                             area that received an eviction judgement in which renters were ordered to 
+                             leave")
+                           ) 
+                  
+                  
+      )
     )
   )
-  )
+)
